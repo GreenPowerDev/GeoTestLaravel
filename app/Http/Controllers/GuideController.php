f88dd3067b662c094d0ca5471037c0db9984fe2e
GreenPower <takahasihideo.dev@gmail.com>
2023-06-20 03:56:34 +0900

    testlogin

--- app/Http/Controllers/GuideController.php
+++ app/Http/Controllers/GuideController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test2user;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\Auth;
 
 class GuideController extends Controller
 {
@@ -18,7 +21,22 @@ class GuideController extends Controller
         return view('area');
     }
     public function my_page(){
-        return view('my_page');
+
+        if(!Auth::check()) return redirect()->route('_login');
+        $test2users = Test2user::where('user_id', Auth::user()->id)->get();
+
+        $reserved_tests = [];
+        foreach($test2users as $test2user){
+            //dd($test2user->allowed_id);
+            if($test2user->allowed_id !== 0 && $test2user->mail_sended === 1){
+                array_push($reserved_tests, $test2user);
+            }
+            else{
+                //if($test2user->allowed_id)
+            }
+        }
+
+        return view('my_page', ['reserved_tests'=>$reserved_tests]);
     }
     public function method(){
         return view('method');
