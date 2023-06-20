d4145c7b3dadb16d0129b3f95dcd5d1e0a2d4e5d
alexanderk21 <alexanderklyagin21@gmail.com>
2023-06-21 01:31:16 +0900

    new-site

---
+++ app/Http/Controllers/GanreController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Ganre;
+use App\Models\Province;
+use App\Models\Test;
+use Carbon\Carbon;
+
+class GanreController extends Controller
+{
+    //
+    public function ganre_selected($id){
+        $ganre = Ganre::find($id);
+        $tests = Test::where('test_date', '>=', Carbon::now())
+            ->where('ganre_id', $id)
+            ->orderBy('test_date')
+            ->paginate(4);
+        return view('ganre', [
+            'tests' => $tests
+        ]);
+    }
+}
--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Province;
+use App\Models\Ganre;
 
 class WelcomeController extends Controller
 {
     //
     public function index(){
         $provinces = Province::take(4)->get();
-        return view('welcome', ['provinces'=>$provinces]);
+        $ganres = Ganre::get();
+        return view('welcome', ['provinces'=>$provinces, 'ganres'=>$ganres]);
     }
 }
