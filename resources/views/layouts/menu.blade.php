0a0978ba101de3f52a3a7854b2e243199b9b3406
alexanderk21 <alexanderklyagin21@gmail.com>
2023-07-10 21:30:50 +0900

    menubar

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>試験サイト</title>
+    <link rel="stylesheet" href="{{asset('css/menu.css')}}">
+    <link rel="stylesheet" href="{{asset('css/toastr.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+    <link rel="stylesheet" href="{{asset('css/fontawesome.css')}}">
+    <link href='https://fonts.googleapis.com/css?family=Yu Gothic' rel='stylesheet'>
+</head>
+
+
+<body>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
+    <header>
+
+        <a class="top_logo" href="{{route('welcome_page')}}">
+            <img src="{{asset('img/menu/top_logo.png')}}" alt="top_logo-image">
+        </a>
+
+        <div class="menubar">
+            <a href="{{route('guide.about_site')}}" class="menu-links">このサイトについて</a>
+            <a href="{{route('search.area')}}" class="menu-links">試験エリア一覧</a>
+            <a href="{{route('guide.method')}}" class="menu-links">試験内容と受験方法</a>
+            <a href="{{route('guide.site_policy')}}" class="menu-links">サイトポリシー</a>
+            <a href="{{route('guide.privacy')}}" class="menu-links">プライバシーポリシー</a>
+        </div>
+
+        <div class="top_btns">
+
+        @if(Auth::check())
+            <a class="squre-btns logout" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><img src="{{asset('img/menu/ico_logout.png')}}" alt="logout-icon"></a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+            <a href="{{route('my_page')}}" class="squre-btns mypage">
+                <img src="{{asset('img/menu/ico_mypage.png')}}" alt="mypage-icon">
+            </a>
+        @else
+            <a href="{{route('_login')}}" class="squre-btns login">
+                <img src="{{asset('img/menu/ico_login.png')}}" alt="login-icon">
+            </a>
+            <a href="{{route('_verifyMailSend')}}" class="squre-btns register">
+                <img src="{{asset('img/menu/ico_register.png')}}" alt="register-icon">
+            </a>
+        @endif
+            <a href="{{route('guide.question')}}" class="squre-btns question">
+                <img src="{{asset('img/menu/ico_question.png')}}" alt="question-icon">
+            </a>
+            <div class="hamburger">
+                <div id="menuToggle">
+                    <input type="checkbox" />
+                    <span></span>
+                    <span></span>
+                    <span></span>
+
+                    <ul id="menu">
+                        <div class="menu-list">
+                            <div class="menu_items customer">
+                                {{-- <img src="{{asset('img/top/mainvisual/header/sp_user_icon.png')}}" alt=""> --}}
+                                <a href="{{route('my_page')}}">マイページ</a>
+                            </div>
+                            <div class="menu_items">
+                                <img src="{{asset('img/top/mainvisual/header/top_go_icon.png')}}" alt="">
+                                <a href="{{route('guide.question')}}">お問合せ</a></div>
+                            <div class="menu_items">
+                                <img src="{{asset('img/top/mainvisual/header/top_go_icon.png')}}" alt="">
+                                <a href="">Q&A</a></div>
+                            <div class="menu_items">
+                                <img src="{{asset('img/top/mainvisual/header/top_go_icon.png')}}" alt="">
+                                <a href="{{route('guide.notice')}}">サイトからのお知らせ</a></div>
+                            <div class="menu_items">
+                                <img src="{{asset('img/top/mainvisual/header/top_go_icon.png')}}" alt="">
+                                <a href="{{route('guide.about_site')}}">このサイトについて</a></div>
+                            <div class="menu_items">
+                                <a href="{{route('guide.site_policy')}}">サイトポリシー</a></div>
+                            <div class="menu_items">
+                                <a href="{{ route('logout') }}">プライバシーポリシー</a></div>
+                            <div class="top_buttons spbtn">
+                        </div>
+                    </ul>
+                </div>
+                <p>メニュー</p>
+            </div>
+        </div>
+    </header>
+
+    <a id="back-to-top"></a>
+
+    @yield('main-content')
+
+    <footer>
+        <a class="footer_logo">
+            <img src="{{asset('img/menu/top_logo.png')}}" alt="">
+        </a>
+        <div class="footer_detail">
+            <a class="footer_links"><i class="fa fa-user" aria-hidden="true" style="font-siza:20px;"></i>お問合せ</a>
+            <a class="footer_links"><i class="fa fa-hand-o-right" aria-hidden="true" style="font-siza:20px;"></i>サイトからのお知らせ</a>
+            <ul><a class="footer_links"><i class="fa fa-hand-o-right" aria-hidden="true" style="font-siza:20px;"></i>このサイトについて</a>
+                <li><a class="footer_links">サイトポリシー</a></li>
+                <li><a class="footer_links">プライバシーポリシー</a></li></ul>
+        </div>
+        <div class="copylight">
+            <p>© <script>document.write(new Date().getFullYear())</script> All Rights Reserved.</p>
+        </div>
+    </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('js/top_scroll_tool.js')}}"></script>
+    <script src="{{asset('js/toastr.js')}}"></script>
+    <script src="{{asset('js/toastr_sr.js')}}"></script>
+    @if(session()->has('message'))
+    <script>
+        toastr.success("{{ session()->get('message') }}");
+    </script>
+    @endif
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,62 @@
-@extends('layouts.main')
+@extends('layouts.menu')
 @section('main-content')
-<link href="{{asset('/css/top_ex_style.css')}}" rel='stylesheet'>
+<link href="{{asset('/css/welcome_style.css')}}" rel='stylesheet'>
     <section class="mainvisual">
         <div class="top_screen">
-            <div class="top_left">
-                <img src="{{asset('img/top/mainvisual/top_background.png')}}" alt="" class="back_pc">
-                <img src="{{asset('img/top/mainvisual/top_sp.jpg')}}" alt="" class="back_sp">
-                <p class="top_up_title"><span style="font-family: 'ComicSansMS3';">Special</span><br><span id='sapn-meister' style="margin-left:100px;font-family: 'ComicSansMS3';">Meister</span></p>
-                <p class="top_down_title">私が地元の<br id="down_title_space"><span id="meister"> Meister</span>です。</p>
+            <img class="bg-pc" src="{{asset('img/users/main_bg.png')}}" alt="background-image">
+            <img class="bg-sp" src="{{asset('img/users/main_bg-sp.jpg')}}" alt="background-image">
+            <div class="top_subject">
+                <p class="top_subject-special">Special</p>    
+                <p class="top_subject-meister">Meister</p>    
             </div>
-            <div class="test_schedul-modal">
-                <a href="#modal-btn" class="schedul_btn"><p style="margin:0; padding:0;">情報</p> </a>
-             </div>
-            <div id="modal-btn" class="top_right" >
-                <div class="test_schedul">
-                    <a href="#" class="top_right__close">&times;</a>          
-                    <div class="sch_title">
-                        インフォメーション
-                    </div>
-                    <div class="sch-content">
-                    @foreach ($notices as $notice)
-                        <div class="sch_details">
-                            <p class="detail_date">{{$notice->get_notice_date()}}</p>
-                            <a href="{{route('notice.select', ['id'=>$notice->id])}}" class="detail_title">{{$notice->notice_contitle}}</a>
-                            <p class="detail_content">{{$notice->notice_context}}</p>
-                        </div>            
-                    @endforeach
-                    </div>
-                    <div class="sch_footer">
-                        <a href="{{route('guide.notice')}}"><p class="list_footer">一覧を見る</p></a>
-                    </div>  
-                </div>
+            <div class="top_idea">
+                私が地元の<span id="top_idea-meister"> Meister</span>です。
             </div>
         </div>
+        
+        <div class="top_notices-modal">
+            <a href="#top_notices" class="schedul_btn"><p style="margin:0; padding:0;">情報</p> </a>
+        </div>
+
+        <div class="top_notices" id="top_notices">
+            <div class="top_notices-title">インフォメーション
+                <a href="#" class="top_notices-close">&times;</a>          
+            </div>
+                
+
+            <div class="top_notices-detail">
+            @foreach ($notices as $notice)
+
+                    <div class="top_notices-content">
+                        <p class="notice-date">{{$notice->get_notice_date()}}</p>
+                        <a href="{{route('notice.select', ['id'=>$notice->id])}}" class="notice-title">{{$notice->notice_contitle}}</a>
+                        <p class="notice-text">{{$notice->notice_context}}</p>
+                    </div>
+        
+            @endforeach
+            </div>
+
+            <a href="{{route('guide.notice')}}"><div class="top_notices-all">一覧を見る</div></a>
+        </div>
+
+        <img class="top_map" src="{{asset('img/users/main_map.png')}}" alt="map-image">
     </section>
 
     
     <section class="promotion">
-        <p class="promotion_title">プロモーション</p>
-        <div class="video">
-            <video controls>
-                <source src="{{asset('img/interview01.mp4')}}" type="video/mp4">
-            </video>
+        <div class="promotion_detail">
+            <img class="promo_girl" src="{{asset('img/users/promo_girl.png')}}" alt="map-image">
+            <img class="promo_camera" src="{{asset('img/users/promo_camera.png')}}" alt="map-image">
+
+            <p class="promotion_title">プロモーション</p>
+
+            <div class="video">
+                <video controls>
+                    <source src="{{asset('img/interview01.mp4')}}" type="video/mp4">
+                </video>
+            </div>
         </div>
+
     </section>
 
     @if(!Auth::check())
@@ -50,35 +64,40 @@
     <section class="free_test">
         <p class="free_test-title">お試し検定</p>
         <p class="free_test-summary">6つの都道府県で各5問お楽しみいただけます！</p>
-        <div class="free_test_parts">
-        @php
-        $i=0;
-        @endphp
-        @foreach($first_tests as $first_test)
+        <div class="free_test-bg">
+            <img class="free_test-trunk" src="{{asset('img/users/trunk.png')}}">
+            <img class="free_test-trunk-right" src="{{asset('img/users/trunk.png')}}">
+            <div class="free_test_parts">
             @php
-            $i++;
+            $i=0;
             @endphp
-            <div class="free_test-part">
-                    <div class="free_test_img">
-                        <img src="{{asset('img/top/test_area/top_free_'.$i.'.jpg')}}" alt="">
-                        <p class="free_test_name">
-                            @foreach(mb_str_split($first_test['province']) as $char)
-                                {{$char}}
-                                <br>
-                            @endforeach
-                            
-                        </p>
-                    </div>
-                    <div class="free_test_detail" id="free_test_detail">
-                        <p class="free_test_detail-ganre">ジャンル：{{$first_test['ganre']}}</p>
-                        <p class="free_test_detail-level">出題数：{{$first_test['count']}}</p>
-                        <div class="free_test_enter">
-                            <a class="free_test_enter_btn" href="{{route('free_test_enter', ["id"=>$first_test['free_id']])}}">検定開始</a>
+            @foreach($first_tests as $first_test)
+                @php
+                $i++;
+                @endphp
+                <div class="free_test-part">
+                        <div class="free_test_img">
+                            <img src="{{asset('img/top/test_area/top_free_'.$i.'.jpg')}}" alt="">
+                            <p class="free_test_name">
+                                @foreach(mb_str_split($first_test['province']) as $char)
+                                    {{$char}}
+                                    <br>
+                                @endforeach
+                                
+                            </p>
                         </div>
-                    </div>
+                        <div class="free_test_detail" id="free_test_detail">
+                            <p class="free_test_detail-ganre">ジャンル：{{$first_test['ganre']}}</p>
+                            <p class="free_test_detail-level">出題数：{{$first_test['count']}}</p>
+                            <div class="free_test_enter">
+                                <a class="free_test_enter_btn" href="{{route('free_test_enter', ["id"=>$first_test['free_id']])}}">検定開始</a>
+                            </div>
+                        </div>
+                </div>
+
+            @endforeach
             </div>
 
-        @endforeach
         </div>
     </section>
 
@@ -88,42 +107,46 @@
 
     <section class="test_area">
         <p class="test_area-title">試験エリア</p>
-        <div class="test_areas">
-            @foreach($provinces as $province)
+        <div class="free_test-bg">
+            <img class="free_test-trunk" src="{{asset('img/users/trunk.png')}}">
+            <img class="free_test-trunk-right" src="{{asset('img/users/trunk.png')}}">
+            <div class="test_areas">
+                @foreach($provinces as $province)
 
-            <div class="test_area-part">
-                <a href="{{route('prefecture.select', ['id'=>$province->id])}}">
-                    <div class="area_img">
-                        <p class="area_name">
+                <div class="test_area-part">
+                    <a href="{{route('prefecture.select', ['id'=>$province->id])}}">
+                        <div class="area_img">
+                            <p class="area_name">
+                                
+                                @foreach(mb_str_split($province->name) as $char)
+                                    {{$char}}
+                                    <br>
+                                @endforeach
+                                
+                            </p>
+                            <img src="{{$province->img_url}}" alt="">
+                        </div>
+                        <div class="area_detail" id="areadetail">
+                            <p class="area_detail-title">最近の開催試験</p>
+                            @if (is_null($province->province_first_test()))
+                            <p class="predetail_title" >テストはありません。</p><br><br><br><br>
+                            @else
                             
-                            @foreach(mb_str_split($province->name) as $char)
-                                {{$char}}
-                                <br>
-                            @endforeach
-                            
-                        </p>
-                        <img src="{{$province->img_url}}" alt="">
-                    </div>
-                    <div class="area_detail" id="areadetail">
-                        <p class="area_detail-title">最近の開催試験</p>
-                        @if (is_null($province->province_first_test()))
-                        <p class="predetail_title" >テストはありません。</p>
-                        @else
+                                <p class="aera_detail_test_title">試験タイトル:{{$province->province_first_test()->name}}</p>
+                                <p class="area_detail-date">開催日：{{$province->province_first_test()->get_test_date()}}</p>
+                                <p class="area_detail-time">開催時間：{{$province->province_first_test()->get_begin_time()}}
+                                                            ～{{$province->province_first_test()->get_end_time()}}</p>
+                                <p class="area_detail-ganre">ジャンル：{{$province->province_first_test()->get_ganre_name()}}</p>
+                                <p class="area_detail-level">レベル：{{$province->province_first_test()->get_level_name()}}</p>
+                            @endif
                         
-                            <p class="aera_detail_test_title">試験タイトル:{{$province->province_first_test()->name}}</p>
-                            <p class="area_detail-date">開催日：{{$province->province_first_test()->get_test_date()}}</p>
-                            <p class="area_detail-time">開催時間：{{$province->province_first_test()->get_begin_time()}}
-                                                        ～{{$province->province_first_test()->get_end_time()}}</p>
-                            <p class="area_detail-ganre">ジャンル：{{$province->province_first_test()->get_ganre_name()}}</p>
-                            <p class="area_detail-level">レベル：{{$province->province_first_test()->get_level_name()}}</p>
-                        @endif
-                    
-                    </div>
-                </a>
+                        </div>
+                    </a>
+                </div>
+
+                @endforeach
+
             </div>
-
-            @endforeach
-
         </div>
         <div class="to_area-list">
             <div>
@@ -135,19 +158,23 @@
     @endif
 
     <section class="sns_media">
+        <img class="sns-girl" src="{{asset('img/users/sns-girl.png')}}">
         <p class="sns-title">SNSメディア</p>
-        <div class="sns-items">
-            <div class="sns-items-img">
-                <a href="#"><img src="{{asset('img/top/sns_media/top_youtube.png')}}" alt=""></a>
-            </div>
-            <div class="sns-items-img">
-                <a href="#"><img src="{{asset('img/top/sns_media/top_twiter.png')}}" alt=""></a>
-            </div>
-            <div class="sns-items-img">
-                <a href="#"><img src="{{asset('img/top/sns_media/top_facebook.png')}}" alt=""></a>
-            </div>
-            <div class="sns-items-img">
-                <a href="#"><img src="{{asset('img/top/sns_media/top_instagram.png')}}" alt=""></a>
+        <div class="sns-bg">
+            <div class="sns-map"></div>
+            <div class="sns-items">
+                <div class="sns-items-img">
+                    <a href="#"><img src="{{asset('img/top/sns_media/top_youtube.png')}}" alt=""></a>
+                </div>
+                <div class="sns-items-img">
+                    <a href="#"><img src="{{asset('img/top/sns_media/top_twiter.png')}}" alt=""></a>
+                </div>
+                <div class="sns-items-img">
+                    <a href="#"><img src="{{asset('img/top/sns_media/top_facebook.png')}}" alt=""></a>
+                </div>
+                <div class="sns-items-img">
+                    <a href="#"><img src="{{asset('img/top/sns_media/top_instagram.png')}}" alt=""></a>
+                </div>
             </div>
         </div>
     </section>
@@ -156,7 +183,9 @@
 
     <section class="search_by_ganre">
         <p class="search_ganre-title">ジャンルから探す</p>
+
         <div class="search_ganre-btns">
+            <img class="search_tree" src="{{asset('img/users/trees.png')}}" alt="">
             @foreach($ganres as $ganre)
 
             <div class="search_each_ganre">
@@ -169,25 +198,48 @@
         </div>
     </section>
     @endif
+
+    <div class="slider">
+            <div id="slider-container">
+                <div id="slider-scroller">
+                    <div class="slider-item"><img src="{{asset('img/users/users_slide(8).png')}}" /></div>
+                    <div class="slider-item"><img src="{{asset('img/users/users_slide(7).png')}}" /></div>
+                    <div class="slider-item"><img src="{{asset('img/users/users_slide(6).png')}}" /></div>
+                    <div class="slider-item"><img src="{{asset('img/users/users_slide(4).png')}}" /></div>
+                    <div class="slider-item"><img src="{{asset('img/users/users_slide(3).png')}}" /></div>
+                    <div class="slider-item"><img src="{{asset('img/users/users_slide(5).png')}}" /></div>
+                    <div class="slider-item"><img src="{{asset('img/users/users_slide(1).png')}}" /></div>
+                    <div class="slider-item"><img src="{{asset('img/users/users_slide(2).png')}}" /></div>
+                </div>
+            </div>
+    </div>
+
+
     <section class="content">
-        <p class="content-title">コンテンツ</p>
         <div class="content-items">
             <div class="content-items-left"><a href="{{route('guide.about_site')}}"><img src="{{asset('img/top/content/top_about.png')}}" alt=""></a>
             </div>
             <div class="content-items-right"><a href="{{route('guide.method')}}"><img src="{{asset('img/top/content/top_method.png')}}" alt=""></a></div>
         </div>
+
     </section>
 
+    <div class="slider">
+            <div id="slider-right-container">
+                <div id="slider-right-scroller">
+                    <div class="slider-right-item"><img src="{{asset('img/users/users_slide(8).png')}}" /></div>
+                    <div class="slider-right-item"><img src="{{asset('img/users/users_slide(7).png')}}" /></div>
+                    <div class="slider-right-item"><img src="{{asset('img/users/users_slide(6).png')}}" /></div>
+                    <div class="slider-right-item"><img src="{{asset('img/users/users_slide(4).png')}}" /></div>
+                    <div class="slider-right-item"><img src="{{asset('img/users/users_slide(3).png')}}" /></div>
+                    <div class="slider-right-item"><img src="{{asset('img/users/users_slide(5).png')}}" /></div>
+                    <div class="slider-right-item"><img src="{{asset('img/users/users_slide(1).png')}}" /></div>
+                    <div class="slider-right-item"><img src="{{asset('img/users/users_slide(2).png')}}" /></div>
+                </div>
+            </div>
+    </div>
 
-<script>
-const areaImg = document.querySelector('.area_img img');
 
-areaImg.addEventListener('load', () => {
-  const areaImgHeight = areaImg.offsetHeight;  
-  const areaDetail = document.getElementById('areadetail');  
-  areaDetail.style.height = areaImgHeight + 'px';
-});
-
-</script>
-    
+<script src="{{asset('js/jquery.js')}}"></script>
+<script src="{{asset('js/slide.js')}}"></script>   
 @endsection
