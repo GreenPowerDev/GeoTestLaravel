f1a6d313b099fc9c08a00e7421e9ba2d3e12e13c
alexanderk21 <alexanderklyagin21@gmail.com>
2023-07-20 17:28:58 +0900

    pay-com

--- app/Http/Controllers/TestPaymantController.php
+++ app/Http/Controllers/TestPaymantController.php
@@ -4,32 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Stripe\Stripe;
+use Stripe\Charge;
 use App\Models\Test;
 
 class TestPaymantController extends Controller
 {
-    //
-    public function payment_apply(Request $request){
+    public function payment_charge(Request $request)
+    {   
         if(!Auth::check()) return redirect()->route('_login');
         $id = $request->id;
-        $test_price = Test::find($id)->price;
-        
-        //dd($test_price);
         if(!$request->agree){
             return redirect()->route('test.apply', $id) ;
         }
-        // return redirect()->route('reserve.add',['id'=>$id, 'test_price'=>$test_price]);
-        $this->charge($id, $test_price);
-    }
-
-    public function payment_charge()
-    {
         Stripe::setApiKey(env('STRIPE_SECRET'));
-
         $token = $request->input('stripeToken');
-        $amount = $test_price; // Amount in cents
-
+        $amount =$request->test_price; // Amount in cents
+        
         try {
             $charge = Charge::create([
                 'amount' => $amount,
@@ -37,7 +28,7 @@ class TestPaymantController extends Controller
                 'description' => 'Example Charge',
                 'source' => $token,
             ]);
-
+            
             // Handle successful payment
             return redirect()->back()->with('message', 'Payment successful!');
         } catch (\Exception $e) {
--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -7,11 +7,12 @@
         
 
             <input type="hidden" value='{{ $test->id }}' name="id">
+            <input type="hidden" value='{{ $test->price }}' name="test_price">
             <p class="terms_title">試験お申込みに対する<br class="sp-lp">利用規約</p>
             <div class="terms_context">
                 <div class="terms_detail">
                     <p>
-                        The quick brown fox jumps over the lazy dog.
+                        Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text
                     </p>
                 </div>
                 <div class="terms_agree">
@@ -24,32 +25,25 @@
         <p class="flow_title">試験お申込みの流れ</p>
         <div class="application_flow_each">
             <p>
-                qwertyuiopaasdfghjklzxcvbnm,.wertyuiodfghj
+                Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text
             </p>
         </div>
         <div class="application_flow_each">
             <p>
-                qwertgfcvvvtrtrcrtcrcdcccdcdrt
+                Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text
             </p>
         </div>
         <div class="application_flow_each">
             <p>
-                dfgfghjgwdfghjertyuiodfghjkgh
+                Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text Dummy Text
             </p>
         </div>
     </section>
     <div class="to_pay">
-        {{-- {{dd($test->price )}} --}}
-        {{-- <button type="submit" class="to_pay_btn" value="決済画面へ"> --}}
-        <script src="https://checkout.stripe.com/checkout.js" class="stripe-button" data-key="{{ env('STRIPE_KEY') }}"
+        <script src="https://checkout.stripe.com/checkout.js" class="stripe-button to_pay_btn" data-key="{{ env('STRIPE_KEY') }}"
             data-amount={{ $test->price }} data-name="総合検定研究所" data-description={{ $test->name }}
-            data-image={{ asset('img/users/meister00.png') }} data-locale="auto" data-label="ssdfe" data-currency="jpy">
+            data-image={{ asset('img/users/meister00.png') }} data-locale="auto" data-label="決済画面へ" data-currency="jpy">
         </script>
     </div>
     </form>
-    <style>
-        .stripe-button-el {
-            width: 800px;
-        }
-    </style>
 @endsection
