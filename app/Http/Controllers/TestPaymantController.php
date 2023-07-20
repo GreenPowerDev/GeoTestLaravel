7a16eded29ac749e93f5748f887440c07529dc38
alexanderk21 <alexanderklyagin21@gmail.com>
2023-07-20 12:04:46 +0900

    get_amount

--- app/Http/Controllers/TestPaymantController.php
+++ app/Http/Controllers/TestPaymantController.php
@@ -5,14 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\Test;
+
 class TestPaymantController extends Controller
 {
     //
     public function payment_apply(Request $request){
         if(!Auth::check()) return redirect()->route('_login');
-        
         $id = $request->id;
-
+        $test_price = Test::find($id)->price;
+        
+        dd($test_price);
         if(!$request->agree){
             return redirect()->route('test.apply', $id) ;
         }
